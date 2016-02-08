a11061056ee3cc640853aeaf0f0310af86e44bc2
hyde2able <hyde31415@gmail.com>
2016-02-08 23:03:23 +0900

    editor.htmlを見てみて

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,39 @@
 $(function(){
-  var articleModal = $(".articleModal")
-  var editorModal = $(".editorModal")
+
+  // 4:3と16:9を変更
+  $("#switch_btn").click(function() {
+    if( $(this).hasClass("by_16_9") ) {
+      $(this).removeClass("by_16_9");
+      $(this).addClass("by_4_3");
+      $(this).text("16 : 9");
+      $(".articles_area").removeClass("by_16_9");
+      $(".articles_area").addClass("by_4_3");
+    } else {
+      $(this).removeClass("by_4_3");
+      $(this).addClass("by_16_9");
+      $(this).text("4 : 3");
+      $(".articles_area").removeClass("by_4_3");
+      $(".articles_area").addClass("by_16_9");
+    }
+  });
+
+  var modal = $("#modal");
   var overlay = $(".overlay")
   var selectedIndex = 0;
 
   $(".editor .articles li").click(function(){
-    editorModal.css({"display": "block"})
-    overlay.css({"display": "block"})
+    modal.fadeIn("normal");
+    overlay.fadeIn("normal");
     selectedIndex = $('.editor .articles li').index(this);
+
+    var html = $('.editor .articles li').eq(selectedIndex).html();
+    $("#modal .preview").html(html);
   });
 
-  $(".addArticle").click(function(){
-    var url = "http://localhost:8080/content?url=" + $('.urlInput').val()
+  // 記事を追加を押したら記事をliに流し込む
+  $("#addArticle").click(function(){
+    var url = "http://localhost:8080/content?url=" + $('.urlInput').val();
+    nowLoading();
     $.ajax({
       url: url,
       type: 'GET',
@@ -19,16 +41,19 @@ $(function(){
     .done(function(data) {
       console.log(data)
       $('.editor .articles li').eq(selectedIndex).html("\
-      <h2>"+ data.title +"</h2>\
-      <p>"+ data.content +"</p>\
-      <p>"+ data.content +"</p>\
-      <p>"+ data.content +"</p>\
-      <p>"+ data.content +"</p>\
-      <p>"+ data.content +"</p>\
-      ")
+        <h2>"+ data.title +"</h2>\
+        <p>"+ data.content +"</p>\
+        <p>"+ data.content +"</p>\
+        <p>"+ data.content +"</p>\
+        <p>"+ data.content +"</p>\
+        <p>"+ data.content +"</p>\
+      ");
+      stopLoading();
+      $('.urlInput').val('');
     })
     .fail(function(data) {
       console.log(data)
+      stopLoading();
     });
   });
 
@@ -39,11 +64,21 @@ $(function(){
   });
 
   $(".overlay").click(function(){
-    articleModal.css({"display": "none"})
-    editorModal.css({"display": "none"})
-    overlay.css({"display": "none"})
+    modal.fadeOut("normal");
+    overlay.fadeOut("normal");
   });
 
   $('#sortable').sortable();
   $('#sortable').disableSelection();
+
+  // ロード中に表示
+  function nowLoading() {
+    $(".loading").show();
+  }
+  function stopLoading() {
+    $(".loading").hide();
+    modal.fadeOut("normal");
+    overlay.fadeOut("normal");
+  }
+
 })
